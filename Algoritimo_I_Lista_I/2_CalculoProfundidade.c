d0306f7fc3c28aa36bc7f998b3b1f73cde006d58
Brendon Esteves <51219353+Unckros@users.noreply.github.com>
2022-05-13 14:59:57 -0400

    Att file

--- Algoritimo_I_Lista_I/2_CalculoProfundidade.c
+++ Algoritimo_I_Lista_I/2_CalculoProfundidade.c
@@ -1,4 +1,5 @@
-/*2 - Uma empresa necessita de um software que consiga calcular a profundidade de um poço artesiano de formato cilindrico com 0.5 metro de raio, de acordo com a capacidade de água pretendida pelo cliente.
+/*2 - Uma empresa necessita de um software que consiga calcular a profundidade de um poço artesiano de formato 
+cilindrico com 0.5 metro de raio, de acordo com a capacidade de água pretendida pelo cliente.
 
 Volume do cilindro em Litros = pi*r²*profund*1000 
 
@@ -8,3 +9,37 @@ Processamento:
 Litros = pi*r²*profund*1000
 =>profund = Litros/(pi*r²*1000)
 =>>profund = Litros/(pi*r*r*1000)*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	const int mil=	1000;
+	const float pi=	3.14;
+	float profundidade;
+	float r;
+	float litros=	0;
+
+	system("clear");
+
+	printf("Informe o valor de R: ");
+	scanf("%f", &r);
+
+	system("clear");
+
+	printf("Informe a profundidade: ");
+	scanf("%f", &profundidade);
+
+	system("clear");
+
+//====================================================
+	litros= pi*(r*r)*profundidade*mil;
+	printf("Volume do cilindro em Litros: \t%.2f\n\n", litros);
+//====================================================
+
+	profundidade= litros/(pi*(r*r)*1000);
+	printf("profundidade:\t %.2f\n", profundidade);
+//====================================================
+
+	return 0;
+}
